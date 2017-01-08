9e2bb3cc7fdc275aede0b2b12cd41bdd388d597c
Jia Li <li.jia3894@gmail.com>
2017-01-07 19:54:36 -0500

    Work in Progress for GUI

--- app/src/main/java/com/example/jj/dementiahelper/AddLocation.java
+++ app/src/main/java/com/example/jj/dementiahelper/AddLocation.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class AddLocation extends AppCompatActivity {
 
@@ -18,8 +20,17 @@ public class AddLocation extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ImageButton addButton = (ImageButton) findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button getLocation = (Button) findViewById(R.id.myLocation);
+        getLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
